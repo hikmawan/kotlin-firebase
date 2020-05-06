78b040b2a3b7b8bac2a6d535ef7cb97f4c2adbac
Ghani Hikmawan <ghani@paxel.co>
2020-05-06 09:38:57 +0700

    firebase authentication

--- app/src/main/java/dev/ghani/firebasedemoapp/MainActivity.kt
+++ app/src/main/java/dev/ghani/firebasedemoapp/MainActivity.kt
@@ -1,18 +1,57 @@
 package dev.ghani.firebasedemoapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     private var mFirebaseAnalytics: FirebaseAnalytics? = null
+    private var mAuth:FirebaseAuth?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Obtain the FirebaseAnalytics instance.
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        // Analytics
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        //Authentication
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    fun buLoginEvent(view: View){
+        val email = etEmail.text.toString()
+        val password = etPassword.text.toString()
+
+        loginToFirebase(email, password)
+    }
+
+    fun loginToFirebase(email:String,password:String){
+        mAuth!!.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this) { task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(applicationContext, "Successful login", Toast.LENGTH_LONG).show()
+                        val currentUser = mAuth!!.currentUser
+                        Log.d("Login : ", currentUser!!.uid)
+                    }else{
+                        Toast.makeText(applicationContext, "Failed login", Toast.LENGTH_LONG).show()
+                    }
+                }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = mAuth!!.currentUser
+
+        if(currentUser!=null){
+            val intent = Intent(this, Control::class.java)
+            startActivity(intent)
+        }
     }
 }
